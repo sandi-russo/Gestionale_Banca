e5e013cc3c00babfcb1b15d6e5619d9b5179c548
sandi-russo <sandirusso0@gmail.com>
2023-12-01 18:54:24 +0100

    Aggiunta File. Creazione Form per registrazione/login utente. Creazione file .CSV e gestione di quest'ultimo.
    Co-authored-by: Samu34l <Samu34l@users.noreply.github.com>

--- Gestionale.c
+++ Gestionale.c
@@ -1,9 +1,28 @@
 #include <stdio.h>
+#include <string.h>
 #include "Consumer.c"
 #include "Administrator.c"
+#include "LoginRegister.c"
 
 void main()
 {
-    printf("\nBenvenuto nella nostra banca!");
-    
+    // Apertura del file in modalità lettura
+    FILE *fpt = fopen("Utenti.csv", "r");
+
+    int scelta;
+    printf("\nBenvenuto nella nostra banca!\n");
+    printf("Cosa vuole fare?\n");
+    printf("1 - Entrare nel conto\n");
+    printf("2 - Aprire un conto\n");
+    scanf("%d", &scelta);
+    switch (scelta)
+    {
+    case 1:
+        Login(fpt);
+        break;
+
+    case 2:
+        Register(fpt);
+        break;
+    }
 }
--- LoginRegister.c
+++ LoginRegister.c
@@ -1,9 +1,69 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <Windows.h>
 
-void Login(){
+// Dichiarazione variabili per il form di Login e di Registrazione
+char Nome[100], Cognome[100], NomeUtente[100], Password[100], ConfermaPassword[100];
 
+void Login(FILE *Utenti)
+{
+    // Inserimento dati utente già registrato
+    // Il system("cls") permette il clear della console su WINDOWS! E non su linux o su altri sistemi operativi. Se si vuole fare per altri sistemi si utilizza system("clear");
+    system("cls");
+    printf("Benvenuto nella schermata di login!\n");
+    printf("Inserisci i tuoi dati per accedere.\n");
+    printf("Nome Utente: ");
+    scanf("%s", &NomeUtente);
+    printf("Password: ");
+    scanf("%s", &Password);
+
+    printf("\nNu:%s, Pass:%s", NomeUtente, Password); // Debug per vedere se stampa i dati corretti
 }
 
-void Register(){
-    
+void Register(FILE *Utenti)
+{
+    Utenti = fopen("Utenti.csv", "w");
+
+    bool fineRegistrazione = false;
+
+    // Iserimento dati utente per registrazione
+    system("cls");
+    printf("Compila il seguente form per aprire il conto!\n");
+    printf("Inserisci il tuo nome: ");
+    scanf("%s", &Nome);
+    printf("Inserisci il tuo cognome: ");
+    scanf("%s", &Cognome);
+    printf("Inserisci il tuo nome utente: ");
+    scanf("%s", &NomeUtente);
+
+    /*
+    Questo do while permette di controllare se la password e la conferma_password combaciano,
+    quindi se entrambe combaciano, manda alla finestra di login.
+    */
+    do
+    {
+        printf("Inserisci una password: ");
+        scanf("%s", &Password);
+        printf("Conferma la password: ");
+        scanf("%s", &ConfermaPassword);
+        int provaPassword = strcmp(Password, ConfermaPassword);
+        if (provaPassword)
+        {
+
+            printf("Le password non combaciano, reinseriscile!\n");
+            Sleep(2000);
+            system("cls");
+        }
+        else
+        {
+            fineRegistrazione = true;
+            printf("Le password combaciano, stai per entrare nella sezione per il login!");
+            Sleep(2000);
+            Login(Utenti);
+        }
+    } while (fineRegistrazione != true);
+    // int provaPassword = strcmp(Password, ConfermaPassword);
+    // printf("\nNome: %s; Cognome: %s; Nome utente: %s; Password: %s; Conferma: %s", Nome, Cognome, NomeUtente, Password, ConfermaPassword);
 }
