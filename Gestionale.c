31f0c48993b8cf12d384c18bfe3b8fb6513edae1
sandi-russo <sandirusso0@gmail.com>
2023-12-10 00:30:08 +0100

    Changes

--- Gestionale.c
+++ Gestionale.c
@@ -9,7 +9,7 @@ int main()
     srand((unsigned int)time(NULL));
     int scelta;
     // Apertura del file in modalità lettura
-    char p1[100] = "Utenti.csv"; // p1 sta per primo percorso file quindi in questo caso stiamo prendendo Utenti.csv
+    char p1[100] = "Utenti.csv"; // p1 sta per primo percorso file. In questo caso, stiamo prendendo "Utenti.csv"
     FILE *fpt;
     controlloFileX(fpt, p1); // Funzione che controlla se il file esiste. Guardare RWCFile.C!
 
