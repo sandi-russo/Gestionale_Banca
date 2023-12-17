31a64aba3878bda64347a0bb70ade8474e60ed40
sandi-russo <sandirusso0@gmail.com>
2023-12-17 11:50:49 +0100

    RWCFile modificato

--- RWCFile.c
+++ RWCFile.c
@@ -18,35 +18,6 @@ void controlloFileX(FILE *f , char percorso[100]) // Controlla se il file è sta
     }
 }
 
-void cFileS1(FILE *f, char a[100], char percorso[100]) // Scrive nel file 1 array di caratteri solo nel caso il file csv contenga una sola colonna;
-{
-    f = fopen(percorso, "a");
-    fprintf(f, "%s\n", a);
-    fclose(f);
-}
-
-void cFileS2(FILE *f, char a[100], char b[100], char percorso[100]) // Scrive nel file 2 array di caratteri solo nel caso il file csv contenga due colonne;
-{
-    f = fopen(percorso, "a");
-    fprintf(f, "%s,%s\n", a, b);
-    fclose(f);
-}
-
-void cFileS3(FILE *f, char a[100], char b[100], char c[100], char percorso[100]) // Scrive nel file 3 array di caratteri solo nel caso il file csv contenga tre colonne;
-{
-    f = fopen(percorso, "a");
-    fprintf(f, "%s,%s, ", a, b);
-    fprintf(f, "%s\n", c);
-    fclose(f);
-}
-
-void cFileS4(FILE *f, char a[100], char b[100], char c[100], char d[100], char percorso[100]) // Scrive nel file 4 array di caratteri solo nel caso il file csv contenga quattro colonne;
-{
-    f = fopen(percorso, "a");
-    fprintf(f, "%s,%s,%s,%s\n", a, b, c, d);
-    fclose(f);
-}
-
 void cFileS5(FILE *f, char a[], char b[], char c[], char d[], char e[], char percorso[100]) // Scrive nel file 5 array di caratteri solo nel caso il file csv contenga cinque colonne;
 {
     //printf("Nome:%s\n Cognome:%s\n NomeUtente:%s\n Password:%s\n ConfermaPassword:%s", a, b, c, d, e);//Riga di debug
