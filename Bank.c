c991a0ee607508aa26c63635952315108e722526
ProdKolod <kolodrecords@gmail.com>
2023-12-28 13:52:49 +0100

    Graphic&Consumer update, PermissionErrorUnrisolved

--- Bank.c
+++ Bank.c
@@ -2,6 +2,8 @@
 
 int main()
 {   
+    system("cls");
+    printf("Caricamento in corso...\n");
     srand((unsigned int)time(NULL)); // Seed randomico per la generazione dell'IBANa
     Banca(); // Richiamo la funzione Banca dalla libreria
     return 0;
